20cd31b58e9a9712ed22db51eab362c66b2f9c35
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-11-04 14:11:18 -0300

    adding 17-2-custom-actions-JS.yaml usando minha custom action em Javascript

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,83 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const setupGit = async () => {
+  await exec.exec(`git config --global user.name "gh-automation"`);
+  await exec.exec(`git config --global user.email "gh-automation@email.com"`);
+};
+
+const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
+
+const setupLogger = ({ debug, prefix } = { debug: false, prefix: '' }) => ({
+  debug: (message) => {
+    if (debug) {
+      core.info(`DEBUG ${prefix}${prefix ? ' : ' : ''}${message}`);
+    }
+  },
+  info: (message) => {
+    core.info(`${prefix}${prefix ? ' : ' : ''}${message}`);
+  },
+  error: (message) => {
+    core.error(`${prefix}${prefix ? ' : ' : ''}${message}`);
+  },
+});
 
 async function run() {
+    const baseBranch =  core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const ghToken = core.getInput('gh-token');
+    const workingDir = core.getInput('working-directory');
+    const debug = core.getBooleanInput('debug');
+
+    core.setSecret(ghToken);
+
+     if (!validateBranchName({ branchName: baseBranch })) {
+    core.setFailed(
+      'Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+    );
+    return;
+  }
+
+  if (!validateBranchName({ branchName: headBranch })) {
+    core.setFailed(
+      'Invalid head-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+    );
+    return;
+  }
+
+  if (!validateDirectoryName({ dirName: workingDir })) {
+    core.setFailed(
+      'Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes.'
+    );
+    return;
+  }
+
+    core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+    core.info(`[js-dependency-update] : target is ${targetBranch}`);
+    core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+
+     await exec.exec('npm update', [], {
+        cwd: workingDir
+    //...commonExecOpts,
+  });
+
+  const gitStatus = await exec.getExecOutput(
+    'git status -s package*.json',
+    [],
+    {
+        cwd: workingDir
+      //...commonExecOpts,
+    }
+  );
+
+
+  if (gitStatus.stdout.length > 0){
+    core.info('[js-dependency-update]: There are updates available!');
+  }else{
+    core.info('[js-dependency-update]: No updates available at this point in time!');
+  }
+  
     /* 
     1.  Parse inputs:
         1.1 base-branch from which to check for updates
